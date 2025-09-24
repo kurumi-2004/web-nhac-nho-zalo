fa58cfbee86f7508312801b44ac2ed171a4dfd44
User <user@example.com>
2025-09-24 18:52:08 +0700

    fix: show home.html as homepage

--- kurumi/urls.py
+++ kurumi/urls.py
@@ -1,12 +1,14 @@
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    # path('', include('users.urls')),  # Đã comment lại vì không có users/urls.py
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
     # path('reminders/', include('reminders.urls')),  # Đã comment lại vì không có reminders/urls.py
     # path('bills/', include('bills.urls')),
     # path('roommates/', include('roommates.urls')),
