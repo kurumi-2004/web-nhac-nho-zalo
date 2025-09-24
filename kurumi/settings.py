7c172d58b27cf5834ef47fa1d994eae9160d47a4
User <user@example.com>
2025-09-24 18:42:48 +0700

    fix: set AUTH_USER_MODEL to users.User for custom user model

--- kurumi/settings.py
+++ kurumi/settings.py
@@ -71,6 +71,7 @@ DATABASES = {
 }
 
 # Password validation
+AUTH_USER_MODEL = 'users.User'
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
